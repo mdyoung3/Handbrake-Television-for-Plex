da643798e8d06cc53a3b9a62c4e4f132f285a9e7
mdyoung3 <mdyoung3@asu.edu>
2017-08-03 05:55:27 -0700

    Working loop for handbrake

--- tvscript.sh
+++ tvscript.sh
@@ -1,12 +1,101 @@
-#/bin/bash
+#!/bin/bash
 
-EP="_s"
-SE="_e"
+#This script takes the titles in a DVD and rips them as individual files
 
-echo "Starting script";
+IAM=$(whoami)
+INFO_SRC="/media/$IAM"
+DVD_NAME=$(ls $INFO_SRC)
+VOB=$INFO_SRC/$DVD_NAME/VIDEO_TS/VIDEO_TS.VOB
+INPUT=/dev/sr0 
+
+handbrake="HandBrakeCLI"
+mediainfo="mediainfo"
+
+echo $handbrake
+
+handbrake_options="--markers --large-file --encoder x264 --encopts vbv-maxrate=25000:vbv-bufsize=31250:ratetol=inf --crop 0:0:0:0 --strict-anamorphic"
+
+width="$(mediainfo --Inform='Video;%Width%' "$VOB")"
+height="$(mediainfo --Inform='Video;%Height%' "$VOB")"
+
+echo $width
+echo $height
+
+if (($width > 1280)) || (($height > 720)); then
+    max_bitrate="5000"
+elif (($width > 720)) || (($height > 576)); then
+    max_bitrate="4000"
+else
+    max_bitrate="1800"
+fi
+
+min_bitrate="$((max_bitrate / 2))"
+
+bitrate="$(mediainfo --Inform='Video;%BitRate%' "$input")"
+
+if [ ! "$bitrate" ]; then
+    bitrate="$(mediainfo --Inform='General;%OverallBitRate%' "$input")"
+    bitrate="$(((bitrate / 10) * 9))"
+fi
+
+if [ "$bitrate" ]; then
+    bitrate="$(((bitrate / 5) * 4))"
+    bitrate="$((bitrate / 1000))"
+    bitrate="$(((bitrate / 100) * 100))"
+
+    if (($bitrate > $max_bitrate)); then
+        bitrate="$max_bitrate"
+    elif (($bitrate < $min_bitrate)); then
+        bitrate="$min_bitrate"
+    fi
+else
+    bitrate="$min_bitrate"
+fi
+
+echo $bitrate
+
+handbrake_options="$handbrake_options --vb $bitrate"
+
+frame_rate="$(mediainfo --Inform='Video;%FrameRate_Original%' "$VOB")"
+
+if [ ! "$frame_rate" ]; then
+    frame_rate="$(mediainfo --Inform='Video;%FrameRate%' "$VOB")"
+fi 
+
+frame_rate_file="$(basename "$VOB")"
+frame_rate_file="${frame_rate_file%\.[^.]*}.txt"
+
+if [ -f "$frame_rate_file" ]; then
+    handbrake_options="$handbrake_options --rate $(cat "$frame_rate_file")"
+elif [ "$frame_rate" == '29.970' ]; then
+    handbrake_options="$handbrake_options --rate 23.976"
+else
+    handbrake_options="$handbrake_options --rate 30 --pfr"
+fi
+
+channels="$(mediainfo --Inform='Audio;%Channels%' "$VOB" | sed 's/[^0-9].*$//')"
+
+if (($channels > 2)); then
+    handbrake_options="$handbrake_options --aencoder ca_aac,copy:ac3"
+elif [ "$(mediainfo --Inform='General;%Audio_Format_List%' "$VOB" | sed 's| /.*||')" == 'AAC' ]; then
+    handbrake_options="$handbrake_options --aencoder copy:aac"
+fi
+
+if [ "$frame_rate" == '29.970' ]; then
+    handbrake_options="$handbrake_options --detelecine"
+fi
+
+lsdvd > dvdinfo.txt
+
+sed -i '/Disc Title:/d' dvdinfo.txt
+sed -i '/Longest track:/d' dvdinfo.txt
 
 echo "What is the name of the show?"
 read show_name
+
+if [ ! -d "$show_name" ]; then
+	mkdir $show_name
+fi
 
 echo "What season is it?"
 read season
@@ -14,22 +103,28 @@ read season
 echo "What is the first episode of the disc?"
 read episode
 
-HandBrakeCLI -t 1 -i /dev/sr0 -o "$show_name$EP$season$SE$episode".mkv -E lame -B 160k -6 stereo -R 48000 -e x264 -x b_adapt=2:bframes=8:direct=auto:fast_pskip=0:deblock=-1,-1:psy-rd=1,0.15:me=umh:me_range=24:partitions=all:ref=16:subq=10:trellis=2:rc_lookahead=60:frameref=15:threads=auto -s1 -2 -v2
-((episode++))
+echo $episode
 
-HandBrakeCLI -t 2 -i /dev/sr0 -o "$show_name$EP$season$SE$episode".mkv -E lame -B 160k -6 stereo -R 48000 -e x264 -x b_adapt=2:bframes=8:direct=auto:fast_pskip=0:deblock=-1,-1:psy-rd=1,0.15:me=umh:me_range=24:partitions=all:ref=16:subq=10:trellis=2:rc_lookahead=60:frameref=15:threads=auto -s1 -2 -v2
-((episode++))
+echo "Encoding: $VOB" >&2
 
-HandBrakeCLI -t 3 -i /dev/sr0 -o "$show_name$EP$season$SE$episode".mkv -E lame -B 160k -6 stereo -R 48000 -e x264 -x b_adapt=2:bframes=8:direct=auto:fast_pskip=0:deblock=-1,-1:psy-rd=1,0.15:me=umh:me_range=24:partitions=all:ref=16:subq=10:trellis=2:rc_lookahead=60:frameref=15:threads=auto -s1 -2 -v2
-((episode++))
+cat dvdinfo.txt | while read line
+do
+	if echo $line | grep -Eq 'Length: 00:0'
+	then
+    	echo 'Crap!'
+	else
+		echo $line
+		title=$(echo $line | awk '{print $2}' | sed 's/^0*//' | tr -dc '[:alnum:]\n\r')
+		output=$show_name/$show_name"_s"$season"_e"$title".mp4"
+ 
+	    time "$handbrake" -t $title \
+    		echo $handbrake_options \
+    		--input "$INPUT" \
+    		--output "$output" \
+    		2>&1 | tee -a "${output}.log"
 
-HandBrakeCLI -t 4 -i /dev/sr0 -o "$show_name$EP$season$SE$episode".mkv -E lame -B 160k -6 stereo -R 48000 -e x264 -x b_adapt=2:bframes=8:direct=auto:fast_pskip=0:deblock=-1,-1:psy-rd=1,0.15:me=umh:me_range=24:partitions=all:ref=16:subq=10:trellis=2:rc_lookahead=60:frameref=15:threads=auto -s1 -2 -v2
-((episode++))
+	fi
 
-HandBrakeCLI -t 5 -i /dev/sr0 -o "$show_name$EP$season$SE$episode".mkv -E lame -B 160k -6 stereo -R 48000 -e x264 -x b_adapt=2:bframes=8:direct=auto:fast_pskip=0:deblock=-1,-1:psy-rd=1,0.15:me=umh:me_range=24:partitions=all:ref=16:subq=10:trellis=2:rc_lookahead=60:frameref=15:threads=auto -s1 -2 -v2
-((episode++))
-
-HandBrakeCLI -t 6 -i /dev/sr0 -o "$show_name$EP$season$SE$episode".mkv -E lame -B 160k -6 stereo -R 48000 -e x264 -x b_adapt=2:bframes=8:direct=auto:fast_pskip=0:deblock=-1,-1:psy-rd=1,0.15:me=umh:me_range=24:partitions=all:ref=16:subq=10:trellis=2:rc_lookahead=60:frameref=15:threads=auto -s1 -2 -v2
-((episode++))
+done
 
 echo "Done."
